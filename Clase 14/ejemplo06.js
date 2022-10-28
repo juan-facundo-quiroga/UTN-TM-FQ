9299f765cef745287b9d4ee677fd2caca68bc182
Facundo <juanfacundoquiroga.91@gmail.com>
2022-10-28 14:59:26 -0300

    Clase 14

---
+++ Clase 14/ejemplo06.js
@@ -0,0 +1,29 @@
+const frutas =['Manzanas','Bananas','Palta','Pera','Mandarinas'];
+
+for(let i=0;i<frutas.length; i++){
+document.write('Tenemos' + frutas[i] + '<br>')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Clase 14/ejemplo08.js
@@ -0,0 +1,37 @@
+function suma(a,b){
+    return a+b;
+}
+
+
+const num1 = 15;
+const num2 = 30;
+const num3 = 20;
+const num4 = 50;
+
+const resulatdo1= suma(num1,num2);
+const resulatdo2= suma(num3,num4);
+
+console.log(resulatdo1);
+console.log(resulatdo2);
+console.log(suma(120,200));
+
+alert(`El primer resulatdo es ${resulatdo1}`);
+alert(`El primer resulatdo es ${resulatdo2}`);
+alert(`El primer resulatdo es ${suma(120,200)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
